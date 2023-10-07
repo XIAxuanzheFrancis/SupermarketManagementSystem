dbb5c77af65b8aab9694bed8a77d2fdbb0d0f333
xiaxuanzhe <3058634081@qq.com>
2023-10-07 14:43:48 +0200

    logout

---
+++ src/main/java/com/xuanzhe/controller/user/LogoutController.java
@@ -0,0 +1,24 @@
+package com.xuanzhe.controller.user;
+
+import com.xuanzhe.util.Constants;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutController extends HttpServlet {
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    req.getSession().removeAttribute(Constants.USER_SESSION);
+    resp.sendRedirect(req.getContextPath()+"/login.jsp");
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    doGet(req, resp);
+  }
+}
